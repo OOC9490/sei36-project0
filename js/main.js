081ac243a00dd626f4939b1fc5a75e8bdc817233
Lawrence Carreon <l.carreon123@gmail.com>
2020-02-25 21:42:27 +1100

    included a hard-coded, unbeatable ai. (Files changed: main.js)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 //object for managing game logic
 const tictactoe = {
     scoreCard: { 1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0], 5: [0, 0], 6: [0, 0], 7: [0, 0], 8: [0, 0] },
+    tilesFilled: { "147": false, "15": false, "168": false, "24": false, "2578": false, "26": false, "348": false, "35": false, "367": false},
     started: false,
     currentPlayer: 0, //0 for player "X", 1 for player "O"
+    leadingPlayer: 0,
     currentMoves: 0,
     drawCheck: false,
+    aiHasMoved: false,
 
     coinFlip: function(){
-        return ((Math.random() > 0.5) ? 1 : 0);
+        return ((Math.random() > 0.5) ? this.leadingPlayer = 1 : this.leadingPlayer = 0);
     },
 
     checkWinDraw: function( boxId, player ){
+        this.tilesFilled[boxId] = true;
         for (let i = 0; i < boxId.length; i++){
             const winState = boxId[i];
             this.scoreCard[ winState ][ player ] += 1;
@@ -24,6 +28,8 @@ const tictactoe = {
         };
         this.currentPlayer = 1 - player;
         ui.playerTurnMsg(this.currentPlayer);
+        if(this.currentPlayer === 1){this.aiTurn()};
+        this.aiHasMoved = false;
     },
 
     resetGame: function(){
@@ -31,8 +37,65 @@ const tictactoe = {
         this.currentMoves = 0;
         this.currentPlayer = this.coinFlip();
         Object.keys(this.scoreCard).forEach(number => this.scoreCard[number] = [0,0]);
+        Object.keys(this.tilesFilled).forEach(key => this.tilesFilled[key] = false);
         ui.playerTurnMsg(this.currentPlayer);
+        this.aiHasMoved = false;
+        if(this.leadingPlayer === 1){this.aiTurn()};
     },
+
+    aiTurn: function(){
+        if(this.currentPlayer === 1){
+            if( this.aiWinOrDenyPlayer(1,2,0) ){return}; //AI prioritises winning over anything else
+            if( this.aiWinOrDenyPlayer(0,2,0) ){return}; //AI then checks for any possible player winning situations
+            if( this.aiWinOrDenyPlayer(1,1,1) ){return}; //AI then checks for any moves that would set up a possible win (so it ignores any lines on the board that already have no chance of winnning)
+            if( this.aiHasMoved === false ){this.aiOpeningOrRandomMove();}; // AI's predetermined first moves or it's in a position where it can't find a winning path
+        };
+    },
+
+    aiWinOrDenyPlayer: function(playerToCheck, valueToCheck, lengthToCheck){
+        if($('#2578').is(':empty') && this.currentMoves > 1){
+            $('#2578').click();
+            return true;
+        };// this forces the most optimal move, taking the centre after a corner on the leading move, if the human doesn't take it
+        const tilesNotFilled = Object.keys(this.tilesFilled).filter(key => this.tilesFilled[key] === false);
+        const scoreKeyArray = Object.keys(this.scoreCard).filter(key => this.scoreCard[key][playerToCheck] === valueToCheck);
+        if ( scoreKeyArray.length > 0){
+            for(let i = 0; i < scoreKeyArray.length; i++){
+                const tileId = scoreKeyArray[i];
+                const affectedTiles = tilesNotFilled.filter(element => element.includes(tileId));
+                if( affectedTiles.length > lengthToCheck){
+                    $(`#${affectedTiles[Math.floor( Math.random() * affectedTiles.length)]}`).click();
+                    this.aiHasMoved = true;
+                    return true; // causes aiTurn function to stop (to prevent the ai from making multiple moves)
+                };
+            };
+        };
+        return false; // lets aiTurn keep going 
+    },
+
+    aiOpeningOrRandomMove: function(){
+        const $middleTile = $('#2578');
+        const $cornerTiles = [];
+        $('#147, #168, #348, #367').each(function(){
+            if($(this).is(':empty')){
+                $cornerTiles.push($(this));
+            };
+        });
+        const $randomCornerTile = $cornerTiles[Math.floor( Math.random() * $cornerTiles.length )];
+        if(this.leadingPlayer === 1 && this.currentMoves < 1){
+            //The AI is the first to move, it will aim for the corners
+            $randomCornerTile.click();
+        }else{
+            //If the AI is second it will aim for the middle tile or take a corner, if the player takes the middle tile going first
+            if($middleTile.is(':empty')){
+                $middleTile.click();
+            }else if($cornerTiles.length > 0){
+                $randomCornerTile.click();
+            }else{
+                $('.box:empty').eq(0).click();
+            };
+        };
+    }
 };
 
 //object for interacting with the user interface
@@ -48,6 +111,7 @@ const ui ={
             ui.$audio[1].play();
             ui.playerTurnMsg(tictactoe.currentPlayer);
             tictactoe.started = true;
+            tictactoe.aiTurn();
         },2000);
     },
 
