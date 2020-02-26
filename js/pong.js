7efc8f1908f0795e0c405660e11e5cbd972aa93d
Lawrence Carreon <l.carreon123@gmail.com>
2020-02-26 11:12:10 +1100

    pong ball collision detection completed

--- js/pong.js
+++ js/pong.js
@@ -11,47 +11,48 @@ class Rectangle{
         this.pos = new Vector;
         this.size = new Vector(w,h);
     };
+    get left(){
+        return this.pos.x - this.size.x / 2;
+    };
+    get right(){
+        return this.pos.x + this.size.x / 2;
+    }; 
+    get top(){
+        return this.pos.y - this.size.y / 2;
+    };
+    get bottom(){
+        return this.pos.y + this.size.y / 2;
+    };
 };
 
 class Ball extends Rectangle{
     constructor(){
         super(5,5);
         this.vel = new Vector;
-    }
+    };
 };
 
 //some setup stuff
 let canvas = document.getElementById("pong");
 const context = canvas.getContext("2d");
-const headerFooterValue = 185;
+const headerFooterValue = 200;
 
 const ball = new Ball;
 ball.pos.x = 100;
 ball.pos.y = 50;
-ball.vel.x = 50;
-ball.vel.y = 50;
+ball.vel.x = 1;
+ball.vel.y = 1;
 
-let lastTime;
+const update = function(){
+    ball.pos.x += ball.vel.x;
+    ball.pos.y += ball.vel.y;
 
-const callback = function(millis){
-    if(lastTime){
-        update((millis - lastTime) / 1000);
+    //collision, puts the ball in bounds before redirection
+    if( ball.left < 0 || ball.right > canvas.width){
+        ball.vel.x = -ball.vel.x;
     }
-    // debugger;
-    lastTime = millis;
-    requestAnimationFrame(callback);
-}//time calculator
-
-const update = function(dt){
-    ball.pos.x += ball.vel.x * dt;
-    ball.pos.y += ball.vel.y * dt;
-
-    //collision
-    if( ball.pos.x < 0 || ball.pos.x > canvas.width){
-        ball.vel.x *= -1;
-    }
-    if( ball.pos.y < 0 || ball.pos.y > canvas.height){
-        ball.vel.y *= -1;
+    if( ball.top < 0 || ball.bottom > canvas.height){
+        ball.vel.y = -ball.vel.y;
     }
 
     context.fillStyle = "#000";
@@ -62,6 +63,7 @@ const update = function(dt){
     requestAnimationFrame(update);
 }; //movement of the ball is relative to the time difference between update callbacks
 
-callback();
+// callback();
+update();
 
 
