87143b55d523336575b277dc6eae80f2894bdc95
Lawrence Carreon <l.carreon123@gmail.com>
2020-02-26 12:09:24 +1100

    ai player and paddles complete

--- js/pong.js
+++ js/pong.js
@@ -11,6 +11,7 @@ class Rectangle{
         this.pos = new Vector;
         this.size = new Vector(w,h);
     };
+    //use getters to be able to calculate the edges of the drawn shapes
     get left(){
         return this.pos.x - this.size.x / 2;
     };
@@ -32,38 +33,83 @@ class Ball extends Rectangle{
     };
 };
 
+class Player extends Rectangle{
+    constructor(){
+        super(10,40);
+        this.score = 0;
+        this.vel = new Vector;
+    }
+}; // player paddles
+
+class Pong{
+    constructor(canvas){
+        this.canvas = canvas;
+        this.context = canvas.getContext("2d");
+        this.ball = new Ball;
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 50;
+        this.ball.vel.x = 100;
+        this.ball.vel.y = 100;
+
+        this.players = [
+            new Player,
+            new Player
+        ];
+        
+        this.players[0].pos.x = 20; //Player1
+        this.players[1].pos.x = canvas.width - 20;  //Player2
+        this.players.forEach(player => player.pos.y = canvas.height / 2);
+
+        //AI speed
+        this.players[1].vel.y = 100;
+
+        let lastTime;
+        const callback = (millis) => {
+            if(lastTime){
+                this.update((millis - lastTime) / 1000 );
+            };
+            lastTime = millis;
+            requestAnimationFrame(callback);
+        };
+        callback();
+    };
+
+    boardRefresh(){
+        this.context.fillStyle = "#000";
+        this.context.fillRect(0, 0 , this.canvas.width, this.canvas.height);
+        this.drawRect(this.ball);
+        this.players.forEach(player => this.drawRect(player));
+    };
+
+    // a generic function for drawing the ball
+    drawRect(rect){
+        this.context.fillStyle = "#fff";
+        this.context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
+    };
+
+    update(dt) {
+        this.ball.pos.x += this.ball.vel.x * dt;
+        this.ball.pos.y += this.ball.vel.y * dt;
+        this.players[1].pos.y += this.players[1].vel.y * dt;
+    
+        //collision detection
+        if( this.ball.left < 0 || this.ball.right > this.canvas.width){
+            this.ball.vel.x = -this.ball.vel.x;
+        }
+        if( this.ball.top < 0 || this.ball.bottom > this.canvas.height){
+            this.ball.vel.y = -this.ball.vel.y;
+        }
+        if( this.players[1].top < 0 || this.players[1].bottom > this.canvas.height){
+            this.players[1].vel.y = -this.players[1].vel.y;
+        }
+        
+        this.boardRefresh();
+    }; 
+}; //game's main class
+
 //some setup stuff
-let canvas = document.getElementById("pong");
-const context = canvas.getContext("2d");
-const headerFooterValue = 200;
+const canvas = document.getElementById("pong");
+const pong = new Pong(canvas);
 
-const ball = new Ball;
-ball.pos.x = 100;
-ball.pos.y = 50;
-ball.vel.x = 1;
-ball.vel.y = 1;
-
-const update = function(){
-    ball.pos.x += ball.vel.x;
-    ball.pos.y += ball.vel.y;
-
-    //collision, puts the ball in bounds before redirection
-    if( ball.left < 0 || ball.right > canvas.width){
-        ball.vel.x = -ball.vel.x;
-    }
-    if( ball.top < 0 || ball.bottom > canvas.height){
-        ball.vel.y = -ball.vel.y;
-    }
-
-    context.fillStyle = "#000";
-    context.fillRect(0, 0 , canvas.width, canvas.height);
-
-    context.fillStyle = "#fff";
-    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-    requestAnimationFrame(update);
-}; //movement of the ball is relative to the time difference between update callbacks
-
-// callback();
-update();
 
 
