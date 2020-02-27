307eb98d189c100541bec433547e7d6e240f7fc3
Lawrence Carreon <l.carreon123@gmail.com>
2020-02-28 07:57:53 +1100

    corrected a syntax error in home.js' matchMedia check

--- js/home.js
+++ js/home.js
@@ -19,7 +19,7 @@ const applyRainbow = function( $element, paddingColor ){
 
 $(document).ready(function(){
     //isMobile query is just an assumption about the max-width of the browser
-    const isMobile = window.matchMedia("max-width: 767px").matches;
+    const isMobile = window.matchMedia("(max-width: 767px)").matches;
     const $startupSound = $("audio");
     $(".clickbait button").on("click", function(){
         $(".clickbait").fadeOut(500).addClass("disabled");
